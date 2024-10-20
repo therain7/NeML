42da9252f044e9345b77f062571d91dbaf419eca
Andrei <therain.i@yahoo.com>
2024-10-20 03:34:22 +0300

    feat: expressions parser

---
+++ lib/parse/expr.mli
@@ -0,0 +1,13 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2024, Andrei, PavlushaSource *)
+
+(** SPDX-License-Identifier: MIT *)
+
+[@@@ocaml.text "/*"]
+
+open! Base
+open Angstrom
+open LAst
+
+val pexpr : expression t
